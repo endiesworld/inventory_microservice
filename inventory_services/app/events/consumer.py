1c8b8dbe119d1a92b545b77680f020792286fe69
endiesworld <endiesworld@gmail.com>
2023-12-12 16:01:11 +0100

    feat: Configured event consumer to fetch product by id

--- inventory_services/app/events/consumer.py
+++ inventory_services/app/events/consumer.py
@@ -1,11 +1,25 @@
+import logging
 import asyncio
-
+from fastapi import Depends
 from app.models.domains.products import redis
+from app.apis.products import fn_get_product_by_id
+from app.db.repositories import ProductsRepository
+from app.db.base import BaseRepository
 
 STREAM_KEY = 'completed_order'
 CONSUMER_GROUP_NAME = 'inventory-group'
 FROM_ID = '>'
 CONSUMER_NAME = 'productt_payment_consumer_1'
+
+
+def get_repository(repo_type: BaseRepository) :
+    from app.models.domains.products import redis
+    def get_repo(db: redis = redis):
+        return repo_type(db)
+
+    return get_repo
+
+product_repo = get_repository(ProductsRepository)()
 
 async def process_redis_stream():
     
@@ -26,9 +40,10 @@ async def process_redis_stream():
         # Process the messages
         for _, messages in reply:
             for message in messages:
-                print("Unprocessed message: ", message)
                 mess = message[1]
-                print(f"processed Message: {mess}")
+                product_id = mess['product_id']
                 redis.xack(STREAM_KEY, CONSUMER_GROUP_NAME, message[0])
+                result = await fn_get_product_by_id(product_id, product_repo)
+                print("Inevtory Record: ", result)
         await asyncio.sleep(1)
         
